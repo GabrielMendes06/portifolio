1afa4e76b419f4ec08263e2183b429e06e9cda2a
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-07-05 12:10:21 -0300

    subindo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,10 +17,10 @@ function Footer() {
                     <p className="texto-footer">Vivendo, aprendendo e subindo de nível um dia de cada vez.</p>
                 </div>
                 <div className="social-icons">
-                    <a href="https://www.linkedin.com/in/gabrielmendes7/" className="button-medium"><img src={Linkedin} className="icon-small"></img></a>
-                    <a href="https://github.com/GabrielMendes06" className="button-medium"><img src={Github} className="icon-small"></img></a>
-                    <a href="mailto:gabriel.mendes17@outlook.com" className="button-medium"><img src={Email} className="icon-small"></img></a>
-                    <a href="https://www.instagram.com/mendezz_gabriel/" className="button-medium"><img src={Instagram} className="icon-small"></img></a>
+                    <a href="https://www.linkedin.com/in/gabrielmendes7/" target="blank" className="button-medium"><img src={Linkedin} className="icon-small"></img></a>
+                    <a href="https://github.com/GabrielMendes06" target="blank" className="button-medium"><img src={Github} className="icon-small"></img></a>
+                    <a href="mailto:gabriel.mendes17@outlook.com" target="blank" className="button-medium"><img src={Email} className="icon-small"></img></a>
+                    <a href="https://www.instagram.com/mendezz_gabriel/" target="blank" className="button-medium"><img src={Instagram} className="icon-small"></img></a>
                 </div>
                 <div className="copyright">
                     <p className="textcopy">Criado por Gabriel Mendes &copy; </p>
