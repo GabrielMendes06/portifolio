46017fdd852e93e8792a39b1ea2275b829325caa
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-07-07 09:54:51 -0300

    add wpp

--- src/Components/MediumPage.js
+++ src/Components/MediumPage.js
@@ -37,7 +37,7 @@ function MediumPage() {
                 <div className="colum-text" style={{paddingTop: "100px", paddingBottom: "250px"}}>
                     <h1 className="titles-black">Achou o visual do meu portifólio agradável ?</h1>
                     <p className="text-p" style={{margin: "40px"}}>Ficarei muito feliz e grato em ter uma oportunidade de trabalhar com você!</p>
-                    <a href="https://www.linkedin.com/in/gabrielmendes7/" className="purple-but" target="blank" style={{margin: "auto", padding: "10px"}}>Começar uma conversa</a>
+                    <a href="https://wa.me/5511977829550" className="purple-but" target="blank" style={{margin: "auto", padding: "10px"}}>Começar uma conversa</a>
                 </div>
             </div>
         </div>
