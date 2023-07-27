9290d354510c2680c8dc22f37aac404e9662c563
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-07-27 12:59:41 -0300

    update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,13 +11,13 @@ function Main() {
         <ContainerEarly
           title={"Desenvolvedor Frontend :)"}
           paragraph={
-            "Eu construo sistemas e designs codificando sempre da maneira mais limpa possível, simplesmente amo oque faço."
+            "Eu construo sistemas e designs codificando sempre da maneira mais limpa possível."
           }
         />     
       <ContainerCartoon img={PurpleTecnology} />
       <MidlePurple
         title="Olá, me chamo Gabriel, prazer em conhece-lo!"
-        paragraph="Comecei a estudar programação no final de 2022, com facilidade em aprender e desejo em adquirir conhecimento, construi uma base sólida o
+        paragraph="Tenho extrema fácilidade em aprender e extremo desejo em adquirir conhecimento. Através de muito esforço e dedicação, construi uma base sólida o
       suficiente para desenvolver sistemas de forma sutil, eficiente e limpa.
       Inclusive, apresento-lhe algumas das minhas habilidades através
       da construção e design desta página, meu portifólio!"
